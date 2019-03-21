6ccf155d1d021812c334866c6bf7895f7a0fd029
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-03-21 07:56:37 -0400

    created else if statements

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,41 +3,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        /*int pickedNumber = (int)(Math.random()*100);
-        System.out.println(pickedNumber);
-        boolean correct = false;
 
-        Scanner keyboard;
-        int numberOfGuesses = 0;
-        int input;
-        keyboard = new Scanner (System.in);
-
-        System.out.println("Guess a number between 1 and 100");
-
-        if (input > pickedNumber) {
-            System.out.println("Guess again, pick a lower number");
-        } else if (input < pickedNumber) {
-            System.out.println("Guess again, pick a higher number");
-        } else if (input == pickedNumber) {
-            System.out.println("The number is correct!");
-
-
-
-
-
-
-
-       }
-    }}*/
-        int secretNumber;
+        int secretNumber = (int)(Math.random()*100);
+        System.out.println(secretNumber);
         int guess;
         boolean correct = false;
 
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Secret Number:");
-        secretNumber = keyboard.nextInt();
         while (!correct);
 
         System.out.println("Guess a number between one and one hundred");
-    }}
+        guess = keyboard.nextInt();
+
+        if (guess > secretNumber) {
+            System.out.println("Guess again, pick a lower number");
+        } else if (guess < secretNumber) {
+            System.out.println("Guess again, pick a higher number");
+        } else if (guess == secretNumber) {
+            correct = true;
+            System.out.println("Correct!");
+    }}}
