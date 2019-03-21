879ccdcf1a33c9505734dfb6ad87d7ed349d3054
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-03-21 07:48:50 -0400

    int secretNumber and guess

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,43 @@
 package com.LickingHeights;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+        /*int pickedNumber = (int)(Math.random()*100);
+        System.out.println(pickedNumber);
+        boolean correct = false;
+
+        Scanner keyboard;
+        int numberOfGuesses = 0;
+        int input;
+        keyboard = new Scanner (System.in);
+
+        System.out.println("Guess a number between 1 and 100");
+
+        if (input > pickedNumber) {
+            System.out.println("Guess again, pick a lower number");
+        } else if (input < pickedNumber) {
+            System.out.println("Guess again, pick a higher number");
+        } else if (input == pickedNumber) {
+            System.out.println("The number is correct!");
+
+
+
+
+
+
+
+       }
+    }}*/
+        int secretNumber;
+        int guess;
+        boolean correct = false;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Secret Number:");
+        secretNumber = keyboard.nextInt();
+        while (!correct);
+
+        System.out.println("Guess a number between one and one hundred");
+    }}
